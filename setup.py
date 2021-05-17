4b3c39ced4d4b65fd2086eed64f8a46b409e01bf
niuliangtao <niuliangtao@qq.com>
2021-05-17 19:59:35 +0800

    add

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 import sys
 from os import path
 
-from notebuild.tool import get_version
+from notebuild.tool import read_version
 from setuptools import find_packages, setup
 
-version_path = path.join(path.abspath(
-    path.dirname(__file__)), 'script/__version__.md')
-
-version = get_version(sys.argv, version_path, step=16)
+version_path = path.join(path.abspath(path.dirname(__file__)), 'script/__version__.md')
+version = read_version( version_path)
 
 install_requires = ['tushare']
 
